e5d1c29d54b7213e2ff002d40bfc3ab799c31a2a
ruben <ruben@ruben-Lenovo>
2013-03-12 14:48:06 +0100

    Test3

--- ETHz_HeadlineGen/src/Extractor.java
+++ ETHz_HeadlineGen/src/Extractor.java
@@ -2,7 +2,8 @@
 public class Extractor {
 
 	public static void main(String args[]){
-		System.out.println("GIT TEST");
+		System.out.println("GIT TEST2");
+		System.out.println("This file is different");
 	}
 	
 }
