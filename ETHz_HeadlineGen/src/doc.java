a705a53129a50bddd3dfdae963555b56a46d9a56
ruben <ruben@ruben-Lenovo>
2013-03-12 15:39:47 +0100

    stan nlp imported and tested

---
+++ ETHz_HeadlineGen/src/doc.java
@@ -0,0 +1,16 @@
+
+public class doc {
+
+	public doc(String docno, String docType, String textType, String cont) {
+		super();
+		this.docno = docno;
+		this.docType = docType;
+		this.textType = textType;
+		this.cont = cont;
+	}
+	public String docno;
+	public String docType;
+	public String textType;
+	public String cont;
+	
+}
