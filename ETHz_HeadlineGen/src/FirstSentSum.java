232ab662225f19705c1d566e1088bcdc142f8d1f
Jared Niederhauser <jarednieder@gmail.com>
2013-03-18 17:35:12 +0100

    Test

--- ETHz_HeadlineGen/src/FirstSentSum.java
+++ ETHz_HeadlineGen/src/FirstSentSum.java
@@ -1,21 +1,25 @@
 import edu.stanford.nlp.pipeline.Annotation;
 
-
 public class FirstSentSum implements Summerizer {
 	private Doc doc;
-	private Annotation anot; 
+	private Annotation anot;
 	private int sumLeng;
-	public FirstSentSum(Doc doc, Annotation anot, int summaryLength){
-		this.doc=doc;
-		this.anot=anot;
-		this.sumLeng=summaryLength;
+
+	public FirstSentSum(Doc doc, Annotation anot, int summaryLength) {
+		this.doc = doc;
+		this.anot = anot;
+		this.sumLeng = summaryLength;
 	}
-	
+
 	@Override
 	public String summary() {
-		//String firstSentence = 
+		// String firstSentence =
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	public void test() {
+
 	}
 
 }
