c1192a375c0dc973cae5bba3036e07fa940039e5
Jared Niederhauser <jarednieder@gmail.com>
2013-03-18 18:38:27 +0100

    Updated FirstSentSum to allow trimming the beginning/end of sentences based on a list of acceptable parts of speech.

--- ETHz_HeadlineGen/src/FirstSentSum.java
+++ ETHz_HeadlineGen/src/FirstSentSum.java
@@ -1,21 +1,21 @@
 import java.util.List;
 
+import edu.stanford.nlp.ling.CoreAnnotations.PartOfSpeechAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
 import edu.stanford.nlp.ling.CoreAnnotations.TokensAnnotation;
 import edu.stanford.nlp.ling.CoreLabel;
-import java.util.List;
-
-import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
 import edu.stanford.nlp.pipeline.Annotation;
-import edu.stanford.nlp.util.CoreMap;
-import java.lang.annotation.AnnotationFormatError;
 import edu.stanford.nlp.util.CoreMap;
 
 public class FirstSentSum implements Summerizer {
+	private static final String[] START_POS = { "NNP", "NN" };
+	private static final String[] END_POS = { "NNP", "NN" };
+
 	private Doc doc;
 	private Annotation anot;
 	private int sumLeng;
-	private byte TEST_VARIABLE;
-    private String firstSent;
+	private String firstSent;
+
 	public FirstSentSum(Doc doc, Annotation anot, int summaryLength) {
 		this.doc = doc;
 		this.anot = anot;
@@ -29,23 +29,47 @@ public class FirstSentSum implements Summerizer {
 		// Jared is the best
 		return null;
 	}
-	private CoreMap getFirstSent(){
+
+	private CoreMap getFirstSent() {
 		CoreMap sentence = anot.get(SentencesAnnotation.class).get(0);
 		return sentence;
 	}
+
 	// Get rid of words that don't fit at the beginning or end of the sentence
-	private void trimSentenceEnds_JARED_BENJAMIN_NIEDERHAUSER(CoreMap sentence) {
+	private void trimSentenceEnds(CoreMap sentence) {
 		List<CoreLabel> tokens = sentence.get(TokensAnnotation.class);
-		trimBeginning_JARED_BENJAMIN_NIEDERHAUSER(tokens);
-		trimEnd_JARED_BENJAMIN_NIEDERHAUSER(tokens);
+		trimBeginning(tokens);
+		trimEnd(tokens);
 	}
 
-	private void trimBeginning_JARED_BENJAMIN_NIEDERHAUSER(
-			List<CoreLabel> tokens) {
+	private void trimBeginning(List<CoreLabel> tokens) {
+		while (tokens.size() > 0) {
+			String pos = tokens.get(0).get(PartOfSpeechAnnotation.class);
 
+			for (String s : START_POS) {
+				if (s.equals(pos)) {
+					return; // Suitable starting token found
+				}
+			}
+
+			// Remove the first token
+			tokens.remove(0);
+		}
 	}
 
-	private void trimEnd_JARED_BENJAMIN_NIEDERHAUSER(List<CoreLabel> tokens) {
+	private void trimEnd(List<CoreLabel> tokens) {
+		while (tokens.size() > 0) {
+			String pos = tokens.get(tokens.size() - 1).get(
+					PartOfSpeechAnnotation.class);
 
+			for (String s : END_POS) {
+				if (s.equals(pos)) {
+					return; // Suitable starting token found
+				}
+			}
+
+			// Remove the last token
+			tokens.remove(tokens.size() - 1);
+		}
 	}
 }
