4b23dc6e77d2be785ebc4b04167c27c42163f7fe
ruben <ruben@ruben-Lenovo>
2013-03-18 17:32:17 +0100

    Empty class

---
+++ ETHz_HeadlineGen/src/FirstSentSum.java
@@ -0,0 +1,21 @@
+import edu.stanford.nlp.pipeline.Annotation;
+
+
+public class FirstSentSum implements Summerizer {
+	private Doc doc;
+	private Annotation anot; 
+	private int sumLeng;
+	public FirstSentSum(Doc doc, Annotation anot, int summaryLength){
+		this.doc=doc;
+		this.anot=anot;
+		this.sumLeng=summaryLength;
+	}
+	
+	@Override
+	public String summary() {
+		//String firstSentence = 
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
